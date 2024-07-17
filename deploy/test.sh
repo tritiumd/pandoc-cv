d7b0711d5ae48845ca544b3bca9699631f0b4e7a
kidclone3 <kid.clone.3@gmail.com>
2024-07-17 15:02:19 +0700

    chore: minor change for run engine

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,4 +1,6 @@
 # Test when update
-bash run_md2html.sh /home/delus/Documents/pandoc-cv-delus/init test
+# bash run_md2html.sh /home/delus/Documents/pandoc-cv-delus/init test
 
-bash run_yaml2md.sh /home/delus/Documents/pandoc-cv-delus/init test2
+# bash run_yaml2md.sh /home/delus/Documents/pandoc-cv-delus/init test2
+
+bash run_yaml2md.sh /home/delus/Documents/pandoc-cv-delus init/vnframe
